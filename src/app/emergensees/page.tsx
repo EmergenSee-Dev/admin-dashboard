1d298df9a0a9cf2a4ff866acb632f468437af882
iamalaboexcel <iamalaboexcel@gmail.com>
2025-02-12 11:42:31 +0100

    deploy

--- src/app/emergensees/page.tsx
+++ src/app/emergensees/page.tsx
@@ -4,7 +4,7 @@ import CaseCard from '@/components/CaseCard';
 import DashboardLayout from '@/components/DashboardLayout';
 import TotalSection from '@/components/TotalSection';
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { title } from 'process';
 
@@ -74,48 +74,50 @@ const Emergensees = () => {
   };
 
   return (
-    <DashboardLayout>
-      <>
-        <TotalSection />
-        <section className='bg-white rounded-xl p-4 mt-4'>
-          <div className='lg:flex w-full justify-between'>
-            <div className='grid lg:grid-cols-6 grid-cols-3 sm:mb-3'>
-              {filters.map((single, index) => (
-                <button
-                  key={index}
-                  onClick={() => handleFilterClick(single.value)}
-                  className='lg:p-3 sm:py-3 sm:mb-2 my-auto rounded-full mr-4 lg:text-xs text-[10px] text-[#9FA4A7] lg:px-4 bg-[#EFEFEF]'
-                >
-                  {single.title}
-                </button>
-              ))}
-            </div>
-            <input
-              type="text"
-              placeholder='Search'
-              className='bg-[#EFEFEF] p-3 rounded-md lg:w-72 w-full'
-            />
-          </div>
-          {upload && (
-            <>
-              <div className='flex justify-between border-b border-[#DFDFDF] py-6'>
-                <p className='uppercase font-bold'>all UPLOADS</p>
-                <p className='font-medium'>{upload.length} UPLOADS</p>
+    <Suspense fallback={<div>Loading...</div>}>
+      <DashboardLayout>
+        <>
+          <TotalSection />
+          <section className='bg-white rounded-xl p-4 mt-4'>
+            <div className='lg:flex w-full justify-between'>
+              <div className='grid lg:grid-cols-6 grid-cols-3 sm:mb-3'>
+                {filters.map((single, index) => (
+                  <button
+                    key={index}
+                    onClick={() => handleFilterClick(single.value)}
+                    className='lg:p-3 sm:py-3 sm:mb-2 my-auto rounded-full mr-4 lg:text-xs text-[10px] text-[#9FA4A7] lg:px-4 bg-[#EFEFEF]'
+                  >
+                    {single.title}
+                  </button>
+                ))}
               </div>
-              {upload.length > 0 ? (
-                upload.map((single, index) => (
-                  <CaseCard key={index} data={single} />
-                ))
-              ) : (
-                <p className='text-center py-6'>
-                  No uploads found for <span className='font-bold'>{active}</span>.
-                </p>
-              )}
-            </>
-          )}
-        </section>
-      </>
-    </DashboardLayout>
+              <input
+                type="text"
+                placeholder='Search'
+                className='bg-[#EFEFEF] p-3 rounded-md lg:w-72 w-full'
+              />
+            </div>
+            {upload && (
+              <>
+                <div className='flex justify-between border-b border-[#DFDFDF] py-6'>
+                  <p className='uppercase font-bold'>all UPLOADS</p>
+                  <p className='font-medium'>{upload.length} UPLOADS</p>
+                </div>
+                {upload.length > 0 ? (
+                  upload.map((single, index) => (
+                    <CaseCard key={index} data={single} />
+                  ))
+                ) : (
+                  <p className='text-center py-6'>
+                    No uploads found for <span className='font-bold'>{active}</span>.
+                  </p>
+                )}
+              </>
+            )}
+          </section>
+        </>
+      </DashboardLayout>
+    </Suspense>
   );
 };
 
